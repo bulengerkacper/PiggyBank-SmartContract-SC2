08791f963b478aa7b9f287615d078d24c2077fdc
bulengerkacper <bulenger.kacper@gmail.com>
2022-01-23 13:50:21 +0100

    code format

--- retirment.sol
+++ retirment.sol
@@ -13,11 +13,11 @@ contract Retirement {
 	event IncomingPayment(address acc, uint256 amount);
 	event OutgoingPayment(address acc, uint256 amount);
 
-    modifier timeConditionFullfiled {
-        require(block.timestamp > startofRetirment);
-        _;
-    }
-       
+	modifier timeConditionFullfiled {
+		require(block.timestamp > startofRetirment);
+		_;
+	}
+
 	constructor(uint _howManyDays) { //lack of default params in solidity :(
 		//convert_to_days=12*60*60*_howManyDays
 		startofRetirment = block.timestamp + _howManyDays;
@@ -25,13 +25,13 @@ contract Retirement {
 	}
 
 	receive() external payable {
-        accounts[msg.sender]=accounts[msg.sender]+(msg.value*0.99);
-        emit IncomingPayment(msg.sender, msg.value);
-        owner.transfer(msg.value*0.01);
+		accounts[msg.sender]=accounts[msg.sender]+(msg.value*0.99);
+		emit IncomingPayment(msg.sender, msg.value);
+		owner.transfer(msg.value*0.01);
 	}
 
 	function take_pension () public timeConditionFullfiled {
-        payable(msg.sender).transfer(accounts[msg.sender]);
-        emit OutgoingPayment(msg.sender, accounts[msg.sender]);
+		payable(msg.sender).transfer(accounts[msg.sender]);
+		emit OutgoingPayment(msg.sender, accounts[msg.sender]);
 	}
 }
